690869d3e4b5d4a6e6d2f2dc0b563ccc973cf1fe
Andres Riancho <andres.riancho@gmail.com>
2013-11-27 13:33:35 -0300

    Adding setup.py in order to be able to easily install this app

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+
+setup(
+      name='trunserver',
+
+      version='0.2',
+      license = 'MIT License',
+      platforms='Linux',
+      
+      description=('Splunk logger sends log messages to splunk directly from'
+                   ' your Python code.'),
+      long_description=open(join(dirname(__file__), 'README.rst')).read(),
+      
+      author='Gregory Armer',
+      url='https://github.com/gregarmer/trunserver/',
+      
+      packages=find_packages(),
+      install_requires=['twisted',],
+      
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+        ],
+      
+       # In order to run this command: python setup.py test
+       test_suite="nose.collector",
+       tests_require="nose",
+     )
